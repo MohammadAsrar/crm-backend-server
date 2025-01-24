2be9832314ab8b7600ec4ea0bab348d6a521556a
Mohammad Asrar Ahmed <mohammadasrar266@gmail.com>
2025-01-25 03:00:19 +0530

    comments updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
-const { Sequelize } = require("sequelize"); // Added import
+const { Sequelize } = require("sequelize");
 
 dotenv.config();
 
